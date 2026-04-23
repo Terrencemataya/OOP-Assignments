3ba8e49036cf0f1e37fe7c62d878fc4a283e6e1b
Terrence_the_nerd <brightmataya1@gmail.com>
2026-04-23 14:33:44 +0200

    Add problem3.cpp for Problem 3

---
+++ Assignment 4/problem3.cpp
@@ -0,0 +1,9 @@
+// C++ Code for Problem 3
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Your code for Problem 3 goes here
+    cout << "Hello, World!" << endl;
+    return 0;
+}
